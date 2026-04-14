8a647c90777f5ecbcc9f38001a5527ad41c66f49
TheBisonAppa <g8rguy2005@gmail.com>
2026-04-13 22:35:25 -0500

    Window Created For Batteria

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <raylib.h>
+
+#include <imgui.h>
+#include <rlimgui.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1920, 1080, "BATERRIA");
 
-	//change to code
+	rlImGuiSetup(true);
 
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+	
+		rlImGuiBegin();
+
+		DrawRectangle(75, 75, 100, 100, { 0,255,0,255 });
+		DrawRectangle(50, 50, 100, 100, { 255,0,0,255 });
+		
+		DrawText("Congrats! You Created The Window For BATERRIA!!", 960, 540, 20, RED);
+
+		rlImGuiEnd();
+
+		EndDrawing();
+		
+	}
+
+	rlImGuiShutdown();
+
+
+	CloseWindow();
 	return 0;
 }
